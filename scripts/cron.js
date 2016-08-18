5b98ec5ec0c50f40dd7a7da1502cf44f00777b8c
YutakaHorikawa <gozutakemaru@gmail.com>
2016-08-18 18:38:52 +0900

    bug fix

--- scripts/cron.js
+++ scripts/cron.js
@@ -1,17 +1,10 @@
 'use strict';
 
-var _momentTimezone = require('moment-timezone');
-
-var _momentTimezone2 = _interopRequireDefault(_momentTimezone);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 var cron = require('node-cron');
-
 
 module.exports = function (robot) {
   new cron.schedule("0 * */1 * * *", function () {
-    var hourMessage = (0, _momentTimezone2.default)().format('h') + "時になったぞ...";
+    var hourMessage = moment().format('h') + "時になったぞ...";
     robot.send({ room: "#general" }, hourMessage);
   }, null, true, "Asia/Tokyo");
 };
--- src/cron.js
+++ src/cron.js
@@ -1,5 +1,4 @@
 var cron = require('node-cron');
-import moment from 'moment-timezone'
 
 module.exports = (robot => {
     new cron.schedule(
