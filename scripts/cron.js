114317ca9d73aa478c7b0c555d899364d626e96b
horilu <horilu@horilu-no-MacBook-Pro.local>
2016-08-21 23:47:01 -0700

    設定変更

--- scripts/cron.js
+++ scripts/cron.js
@@ -4,8 +4,9 @@ var cron = require('node-cron');
 var moment = require('moment');
 
 module.exports = function (robot) {
-  new cron.schedule("0 */1 * * * *", function () {
-    var hourMessage = moment().format('h') + "時になったぞ...";
-    robot.send({ room: "#general" }, hourMessage);
-  }, null, true, "Asia/Tokyo");
+    console.log('run');
+    new cron.schedule("0 0 0-23 * * *", function () {
+        var hourMessage = moment().format('h') + "時になったぞ...";
+        robot.send({ room: "#general" }, hourMessage);
+    }, null, true, "Asia/Tokyo");
 };
--- src/cron.js
+++ src/cron.js
@@ -1,17 +1,17 @@
 var cron = require('node-cron');
 var moment = require('moment');
 
-// hoge
 module.exports = (robot => {
+    console.log('run');
     new cron.schedule(
-      "0 */1 * * * *",
-      () => {
+        "0 0 0-23 * * *",
+        () => {
             const hourMessage = moment().format('h') + "時になったぞ...";
             robot.send({ room: "#general" }, hourMessage);
-      },
-      null,
-      true,
-      "Asia/Tokyo"
+        },
+        null,
+        true,
+        "Asia/Tokyo"
     );
 });
 
