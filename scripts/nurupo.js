cc86d68c427c87c964618552da578d806dc272f3
YutakaHorikawa <gozutakemaru@gmail.com>
2016-08-24 22:13:20 -0700

    笑うなコマンド

--- scripts/nurupo.js
+++ scripts/nurupo.js
@@ -4,4 +4,8 @@ module.exports = function (robot) {
     robot.hear(/ぬるぽ/i, function (res) {
         res.send("ガッ");
     });
+
+    robot.hear(/www/i, function (res) {
+        res.send("...笑うな！");
+    });
 };
--- src/nurupo.js
+++ src/nurupo.js
@@ -2,5 +2,10 @@ module.exports = (robot => {
     robot.hear(/ぬるぽ/i, res => {
         res.send("ガッ")
     })
+
+    robot.hear(/www/i, res => {
+        res.send("...笑うな！")
+    })
+
 })
 
